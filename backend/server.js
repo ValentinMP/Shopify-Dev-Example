d96749af5cc57b135080b8d387b44f6df46b2448
ValentinMP <valentin.miculescu@sap.com>
2025-01-29 14:09:19 +0200

    Add data seeder for DB and product routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
  import express from 'express';
- import products from './data/products.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 dotenv.config();
 const port = process.env.PORT || 5000;
 
@@ -14,14 +14,9 @@ app.get('/', (req, res) => {
     
     });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
